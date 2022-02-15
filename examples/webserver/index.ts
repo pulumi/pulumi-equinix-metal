74e4e4cf87a642b425bdd13f8aa8522b36eb1398
stack72 <public@paulstack.co.uk>
2022-02-15 15:54:22 +0000

    [internal] Change acceptance tests to take advantage of the new metros

--- examples/webserver/index.ts
+++ examples/webserver/index.ts
@@ -4,11 +4,11 @@ import * as metal from "@pulumi/equinix-metal";
 const project = metal.getProject({ name: "ci-project"}).then( x=> x.id);
 
 const vm = new metal.Device("vm", {
-    facilities: [metal.Facility.EWR1],
+    metro: "da",
     billingCycle: metal.BillingCycle.Hourly,
     hostname: "demoinstance",
-    operatingSystem: metal.OperatingSystem.Ubuntu1804,
-    plan: metal.Plan.T1SmallX86,
+    operatingSystem: metal.OperatingSystem.Ubuntu2004,
+    plan: metal.Plan.C3SmallX86,
     projectId: project,
 });
 
